4d947f5eac9213b8b277e84e13f1791d1184924c
Tim Martin <timmartin13@gmail.com>
2021-03-28 09:47:38 -0500

    A simple FAQ page

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,25 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', function() {
+        // remove non-selected text
+        questions.forEach(function(item) {
+            if (item !== question) {
+                item.classList.remove('show-text');
+            }
+        });
+        question.classList.toggle('show-text');
+    });
+});
+
 // traversing the dom
+// const questionBtns = document.querySelectorAll('.question-btn');
+
+// questionBtns.forEach(function(btn) {
+//     btn.addEventListener('click', function(e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
